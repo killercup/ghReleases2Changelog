a4dd1a61ff1c258a98a3d79da92c5dabbd4be9bc
Pascal Hertleif <killercup@gmail.com>
2014-02-10 13:27:28 +0100

    Generated documentation for 1517cd12231cbe59097ecbe94dc9a3bdf412f131

---
+++ toc.js
@@ -0,0 +1,88 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "Readme.md",
+  "originalPath": "Readme.md",
+  "name": "index.html",
+  "title": "Github Releases to Changelog File",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "github-releases-to-changelog-file",
+      "title": "Github Releases to Changelog File"
+    },
+    {
+      "level": 2,
+      "slug": "install",
+      "title": "Install"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "license",
+      "title": "License"
+    }
+  ]
+},
+{
+  "path": "index.js.html",
+  "originalName": "index.js",
+  "originalPath": "index.js",
+  "name": "index.js.html",
+  "lang": "javascript",
+  "toc": []
+},
+{
+  "path": "bin/releases2changelog.html",
+  "originalName": "releases2changelog",
+  "originalPath": "bin/releases2changelog",
+  "name": "releases2changelog.html",
+  "title": "Github Releases to Changelog File",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "github-releases-to-changelog-file",
+      "title": "Github Releases to Changelog File"
+    },
+    {
+      "level": 2,
+      "slug": "cli",
+      "title": "CLI"
+    },
+    {
+      "level": 3,
+      "slug": "display-help",
+      "title": "Display Help"
+    },
+    {
+      "level": 3,
+      "slug": "display-version",
+      "title": "Display Version"
+    },
+    {
+      "level": 2,
+      "slug": "request",
+      "title": "Request"
+    }
+  ]
+},
+{
+  "path": "lib/convert.js.html",
+  "originalName": "convert.js",
+  "originalPath": "lib/convert.js",
+  "name": "convert.js.html",
+  "lang": "javascript",
+  "title": "Convert Release.json to Changes.md",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "convert-release-json-to-changes-md",
+      "title": "Convert Release.json to Changes.md"
+    }
+  ]
+}
+];
